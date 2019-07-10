9ec7cbdaf97c8ce7e6c65abe8b31463cce04c3e3
DK\IEKA <ieka@sitecore.net>
2019-07-10 12:27:13 +0300

    Fix the issue with not changing the template in the clone items.

---
+++ src/Sitecore.Support.342776/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.342776")]
+[assembly: AssemblyProduct("Sitecore.Support.342776")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.342776/XA/Foundation/Multisite/Pipelines/PushCloneChanges/PushPropertyChanges.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.XA.Foundation.Multisite.Pipelines.PushCloneChanges;
+
+namespace Sitecore.Support.XA.Foundation.Multisite.Pipelines.PushCloneChanges
+{
+    public class PushPropertyChanges : PushCloneChangesProcessorBase
+    {
+        public override void Process(PushCloneChangesArgs args)
+        {
+            Dictionary<string, PropertyChange> propertyChanges = args.Changes.Properties;
+            var keys = propertyChanges.Keys;
+            var values = propertyChanges.Values;
+            foreach (var key in propertyChanges.Keys)
+            {
+                if (key.ToLower().Equals("templateid"))
+                {
+                    args.Clone.Editing.BeginEdit();
+                    args.Clone.TemplateID = (ID)propertyChanges[key].Value;
+                    args.Clone.Editing.EndEdit();
+                }
+            }
+        }
+    }
+}
